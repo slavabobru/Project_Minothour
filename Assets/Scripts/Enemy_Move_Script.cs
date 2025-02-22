511e2a859c0a8610d757e74a66bac4b0eb98741e
kaapyth0n <nikita.kapitonov@gmail.com>
2025-02-22 12:28:56 +0100

    Initial commit

---
+++ Assets/Scripts/Enemy_Move_Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move_Script : MonoBehaviour
+{
+    private Transform plPos;
+    private Vector3 direction;
+    public float enSpeed = 4f;
+    public float attackRng = 8f;
+    private float plDistance;
+
+    void Start()
+    {
+        plPos = GameObject.Find("SUS").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plDistance = Vector3.Distance(transform.position, plPos.position);
+
+        if (plDistance <= attackRng)
+        {
+            direction = (plPos.position - transform.position).normalized;
+            transform.Translate(direction * Time.deltaTime * enSpeed);
+        }
+    }
+}
