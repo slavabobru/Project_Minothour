efaf52a6e8a812a880cc6260036c678cd648eb77
kaapyth0n <nikita.kapitonov@gmail.com>
2025-04-12 14:52:34 +0200

    Fog and Scream UPD prt. 2

--- Assets/Scripts/Screamer_Ghoust_Script.cs
+++ Assets/Scripts/Screamer_Ghoust_Script.cs
@@ -1,16 +1,37 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Screamer_Ghoust_Script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject colider;
+    public GameObject scream_Pos;
+    public GameObject black_Guy;
+    public AudioClip scream;
+    public bool value = false;
 
-    // Update is called once per frame
-    void Update()
+    void ScreamofFun()
     {
-        
+        black_Guy.transform.position = scream_Pos.transform.position;
+        GetComponent<AudioSource>().PlayOneShot(scream);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Scr1"))
+        {
+            Destroy(black_Guy);
+        }
+    }
+    private void OnCollisionEnter(Collision collision) // checks if player toches key pad
+    {
+        if (collision.gameObject.CompareTag("ScrKey1") && value == false)
+        {
+            value = true;
+            ScreamofFun();
+            print("OMG");
+            
+        }
+
     }
 }
